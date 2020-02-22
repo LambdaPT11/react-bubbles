01e00fda44f3df199577c04cd1c5bdfde7bd2247
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-02-21 22:25:19 -0500

    commit before trying login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,50 @@
 import React from "react";
+import axiosWithAuth from './utilis/axiosWithAuth';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+
+  const userHandler = (e) => {
+    setUser(e.target.value)
+  };
+  
+  const passHandler = (e) => {
+    setPass(e.target.value)
+  };
+
+  const login = (e) => {
+    e.preventDefault = {
+      username: user,
+      password: pass
+    }
+    axiosWithAuth()
+      .post('/login', credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubble-page')
+      })
+      .catch(err => console.error('axios:', err))
+  }
+  
   return (
     <div>
-            <form>
+            <form onSubmit={login} className="form">
                 <input
                     type="text"
                     placeholder="User Name"
-                    className="input" />
+                    className="input"
+                    value={user}
+                    onChange={userHandler} />
                 <input
                     type="password"
                     placeholder="Password"
-                    className="input" />
-                <button>Log In</button>
+                    className="input"
+                    value={pass}
+                    onChange={passHandler} />
+                <button className="button">Log In</button>
             </form>
         </div>
   );
