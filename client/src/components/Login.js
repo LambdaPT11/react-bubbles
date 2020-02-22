6044fe9ee4c8fa064b74a757cff3cb0360e18a1e
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-02-21 22:15:35 -0500

    found Login form, looked at bubbles?

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,19 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="User Name"
+                    className="input" />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="input" />
+                <button>Log In</button>
+            </form>
+        </div>
   );
 };
 
